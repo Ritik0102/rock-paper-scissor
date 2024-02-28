740c3101d4df02c0b1612d69e7c1867c1b9a50fa
Ritik <ritik.singh1102@gmail.com>
2024-02-28 23:22:24 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import Home from './component/Home'
 import Game from './component/Game';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
 
   return (
     <>
-    <BrowserRouter basename='/rock-paper-scissor'>
     <Routes>
       <Route exact path='/' element={<Home/>}></Route>
       <Route path='/game' element={<Game/>}></Route>
     </Routes>
-    </BrowserRouter>
 
       
     </>
