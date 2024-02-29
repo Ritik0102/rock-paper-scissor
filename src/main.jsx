db978ff9e7e6f0becdcfc617c43bade9d461ad13
Ritik <ritik.singh1102@gmail.com>
2024-02-29 15:51:05 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/rpck-paper-scissor'>
+    <BrowserRouter basename='/rock-paper-scissor'>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
